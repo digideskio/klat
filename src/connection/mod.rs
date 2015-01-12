76f43130d1a0fcecaf7d951d784abf194a128c94
Carter Hinsley <carterhinsley@gmail.com>
2015-01-12 01:36:40 -0500

    change whitespace from tabs to spaces

--- src/connection/mod.rs
+++ src/connection/mod.rs
@@ -3,6 +3,6 @@ pub mod client;
 pub mod server;
 
 pub trait HasSocket {
-	fn send(&mut self, String);
-	fn recv(&mut self) -> String;
+    fn send(&mut self, String);
+    fn recv(&mut self) -> String;
 }
