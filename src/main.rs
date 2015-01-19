f2de0be74db8923ab20167c00c19c308de96e444
Carter Hinsley <carterhinsley@gmail.com>
2015-01-19 12:31:38 -0500

    (cleanup) conform to 80 column width cap

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use connection::HasSocket;
 mod connection;
 
 fn main() {
-    let mut client = connection::client::ClientConnection { socket: connection::client::client_connect("173.230.156.174", 80) };
+    let mut client = connection::client::ClientConnection {
+		socket: connection::client::client_connect("173.230.156.174", 80)
+		};
     client.send("GET / HTTP/1.1\r\nHost: 173.230.156.174\r\n\r\n".to_string());
     println!("{}", client.recv());
 }
