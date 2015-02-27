da94fb11fc89456120d35bf4856d3c92ca543f5a
Carter Hinsley <carterhinsley@gmail.com>
2015-02-27 10:46:05 +0000

    (refactor) Switch to Haskell

---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Main
+Description : Entry point for Klat
+Copyright   : (c) Carter Hinsley, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
+
+main :: IO ()
+main = do
+    return ()
